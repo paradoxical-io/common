a4535603e6474662558c895e95d8647711a541fa
Jake Swenson <jswenson@godaddy.com>
2016-04-21 07:19:33 -0700

    messed up vars

--- setup-travis.sh
+++ setup-travis.sh
@@ -10,12 +10,12 @@ travis encrypt "SONATYPE_PASSWORD=${SONATYPE_PASSWORD}" -a
 travis encrypt-file gpg/paradoxical-io-private.gpg gpg/paradoxical-io-private.gpg.enc \
     -w gpg/paradoxical-io-private.gpg -p
 
+echo "use \$GPG_PRIVATE_KEY_ENCRYPTION_KEY and \$GPG_PRIVATE_KEY_ENCRYPTION_IV instead"
+
+
 read -p "GPG_PRIVATE_KEY_ENCRYPTION_KEY=" GPG_PRIVATE_KEY_ENCRYPTION_KEY
 read -p "GPG_PRIVATE_KEY_ENCRYPTION_IV=" GPG_PRIVATE_KEY_ENCRYPTION_IV
 
-
-travis encrypt "SONATYPE_PASSWORD=${SONATYPE_PASSWORD}" -a
-travis encrypt "SONATYPE_PASSWORD=${SONATYPE_PASSWORD}" -a
-
-echo "use \$GPG_PRIVATE_KEY_ENCRYPTION_KEY and \$GPG_PRIVATE_KEY_ENCRYPTION_IV instead"
+travis encrypt "GPG_PRIVATE_KEY_ENCRYPTION_KEY=${SONATYPE_PASSWORD}" -a
+travis encrypt "GPG_PRIVATE_KEY_ENCRYPTION_IV=${SONATYPE_PASSWORD}" -a
 

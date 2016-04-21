9635f010e6efc68d8ec254e7a39ada33b7d3b694
Jake Swenson <jswenson@godaddy.com>
2016-04-21 15:47:25 -0700

    using correct sonatype

--- setup-travis.sh
+++ setup-travis.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-read -p "SONATYPE_PASSWORD=" SONATYPE_PASSWORD
-read -p "GPG_PASSWORD=" GPG_PASSWORD
+if [ -z "${SONATYPE_PASSWORD}" ]; then
+    read -p "SONATYPE_PASSWORD=" SONATYPE_PASSWORD
+fi
+
+if [ -z "${GPG_PASSWORD}" ]; then
+    read -p "GPG_PASSWORD=" GPG_PASSWORD
+fi
 
 travis encrypt "SONATYPE_PASSWORD='${SONATYPE_PASSWORD}'" -a
 travis encrypt "GPG_PASSWORD='${GPG_PASSWORD}'" -a
@@ -10,7 +15,7 @@ travis encrypt "GPG_PASSWORD='${GPG_PASSWORD}'" -a
 travis encrypt-file gpg/paradoxical-io-private.gpg gpg/paradoxical-io-private.gpg.enc \
     -w gpg/paradoxical-io-private.gpg -p
 
-echo "use \$GPG_PRIVATE_KEY_ENCRYPTION_KEY and \$GPG_PRIVATE_KEY_ENCRYPTION_IV instead"
+echo "use \$GPG_PRIVATE_KEY_ENCRYPTION_KEY and \$GPG_PRIVATE_KEY_ENCRYPTION_IV for the file encryption command instead"
 
 
 read -p "GPG_PRIVATE_KEY_ENCRYPTION_KEY=" GPG_PRIVATE_KEY_ENCRYPTION_KEY
